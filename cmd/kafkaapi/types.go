4dd9db01ae974dc5a1c9e2c797ad4efe7e1fbd77
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-08-18 14:39:55 +0300

    chore: Модифицировать код подключения kafka 	modified:   Dockerfile 	modified:   cmd/kafkaapi/app.go 	modified:   cmd/kafkaapi/types.go 	modified:   go.mod 	modified:   go.sum
    	modified:   Dockerfile
    	modified:   cmd/kafkaapi/app.go
    	modified:   cmd/kafkaapi/types.go
    	modified:   go.mod
    	modified:   go.sum

--- cmd/kafkaapi/types.go
+++ cmd/kafkaapi/types.go
@@ -1,7 +1,7 @@
 package kafkaapi
 
 import (
-	kaffka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 
 	"github.com/av-belyakov/placeholder_doc-basedb_bi.zone/interfaces"
 )
@@ -10,7 +10,7 @@ import (
 type kafkaApiModule struct {
 	counter      interfaces.Counter
 	logger       interfaces.Logger
-	consumer     *kaffka.Consumer
+	consumer     *kafka.Consumer
 	topics       map[string]string
 	settings     kafkaApiSettings
 	chFromModule chan SettingsChanOutput
